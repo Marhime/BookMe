e77f23ac37201edd2a4c4bcbf0abd9463666ca91
adballz <adnane@MacBook-Pro-de-Ghalem.local>
2018-04-12 12:07:51 +0200

    Commit Before Modifying Controller

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -1,19 +1,21 @@
-git<?php
+<?php
 
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+// Event Controller
 class EventController extends Controller
 {
     /**
      * @Route("/event", name="event")
      */
-    public function index()
+    public function displayEvent()
     {
-        return $this->render('event/index.html.twig', [
+        return $this->render('event/event.html.twig', [
             'controller_name' => 'EventController',
         ]);
     }
+    
 }
