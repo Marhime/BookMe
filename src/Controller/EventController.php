73a1fa97dcfe68da2360611da3a86d762ede0106
adballz <ghalemadnane@gmail.com>
2018-04-12 17:10:12 +0200

    AddEventForm

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -24,4 +24,34 @@ class EventController extends Controller
         ]);
     }
     
+    public function addEditEvent(Request $request)
+    {
+        // 1-Créer un nouvel événement
+        
+        $event = new Event();
+        $event->setTask('Write a blog post');
+        $event->setDueDate(new \DateTime('tomorrow'));
+
+        $form = $this->createFormBuilder($event)
+            ->add('event', TextType::class)
+            ->add('dueDate', DateType::class)
+            ->add('name')
+            ->add('id_user')
+            ->add('place')
+            ->add('opening_date')
+            ->add('closing_date')
+            ->add('phone')
+            ->add('theme')
+            ->add('website')
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+        return $this->render('default/new.html.twig', array(
+            'form' => $form->createView(),
+        ));
+        
+        
+    }
+            
+    
 }
